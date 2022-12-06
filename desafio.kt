7fdafc5c558400d3a29ae836104e6a7f7ab054b0
Salevieno <59671339+Salevieno@users.noreply.github.com>
2022-12-06 10:45:49 -0300

    Resolução do desafio da DIO no bootcamp Kotlin Experience: Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// developed with love by: Salevieno
+// december 06, 2022
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario (val nome : String, val id : Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel)
+{
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Usuario>()	// inscritos is immutable, but its list is mutable, so we can add users
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario)
+    {
+        for (user in usuario)
+        	inscritos.add(user)
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun main()
+{
+    // this function will simulate the creation of a formation and enrollment of two users
+    
+    // creating users
+    val user1 = Usuario("Adam", 1)
+    val user2 = Usuario("Mark", 2)
+    println(user1.toString())
+    println(user2.toString())
+    
+    // creating educational content
+    val contEdu1 = ConteudoEducacional("Primeiros passos com Kotlin", 60)
+    val contEdu2 = ConteudoEducacional("Sintaxe Kotlin", 120)
+    val contEdu3 = ConteudoEducacional("Kotlin avançado", 60)
+    
+    // creating formations
+    val KotlinContEdu = listOf(contEdu1, contEdu2, contEdu3)
+    val formacaoKotlin = Formacao("Kotlin", KotlinContEdu, Nivel.AVANÇADO)
+    
+    // enrolling users in formationKotlin
+    formacaoKotlin.matricular(user1, user2)
+    
+    // printing the results
+    println(user1.toString())
+    println(user2.toString())
+    println()
+    
+    println(contEdu1.toString())
+    println(contEdu2.toString())
+    println(contEdu3.toString())
+    println()
+    
+    println(formacaoKotlin.toString())
+    println(formacaoKotlin.inscritos)
 }
